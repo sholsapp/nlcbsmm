40a6e490796f04a2d08b37a3e028919e1bf886bb
Stephen Holsapple <sholsapp@gmail.com>
2012-01-18 12:56:43 -0800

    Added a blocking system call (read to named pipe) to start program execution.  Will let us bootstrap the DSM before letting the main app run.

--- experiments/benchmark/benchmark.cpp
+++ experiments/benchmark/benchmark.cpp
@@ -17,30 +17,55 @@ void* incer(void *arg) {
    }
 }
 
+#include <sys/types.h>
+#include <sys/stat.h>
+void blocking_entry(void) {
+   /**
+    * Block until data read from named pipe.
+    */
+   FILE* fp;
+   mkfifo("go-pipe", 0755);
+   fp = fopen("go-pipe", "r");
+   fprintf(stderr, "> Waiting for unblock signal\n");
+   fgetc(fp);
+}
 
 int main(void) {
-   int        ar[ARR_SZ];
+
+   blocking_entry();
+
+   int*        ar;
    long         i;
    pthread_t  th1;
    subarray   sb1;
 
+   /**
+    *
+    */
    fprintf(stdout, "> Initializing data...");
+   ar = (int*) malloc (sizeof(int) * ARR_SZ);
    for (i = 0; i < ARR_SZ; i++) {
-     ar[i] = 666;
+      ar[i] = 666;
    }
    fprintf(stdout, "done.\n");
 
+   /**
+    *
+    */
    fprintf(stdout, "> Spawning pthread...");
    sb1.ar = &ar[0];
-   sb1.n  = 500000;
+   sb1.n  = ARR_SZ;
    (void) pthread_create(&th1, NULL, incer, &sb1);
    fprintf(stdout, "done.\n");
 
+   /**
+    *
+    */
    fprintf(stdout, "> Waiting for pthread...");
    (void) pthread_join(th1, NULL);
    fprintf(stdout, "done.\n");
 
-   fprintf(stderr, "Result: ar[666] = %d.\n", ar[666]);
+   fprintf(stderr, "Result: ar[ARR_SZ / 2] = %d.\n", ar[ARR_SZ / 2]);
 
    return 0;
 }
