1550679adc7003b0c07b74058c0ea389fe21f63c
Ryan V <rverdon@ubuntu.(none)>
2012-01-30 19:10:16 -0800

    Added stuff

--- src/nlcbsmm/nlcbsmmheap.h
+++ src/nlcbsmm/nlcbsmmheap.h
@@ -68,164 +68,189 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 namespace HL {
 
-   template<int heap_identifier>
-      class NlcbsmmChunkHeap {
-         /**
-          * The NlcbsmmChunkHeap is a chunk heap that grabs memory from a specific
-          * region of memory, depending on the template heap_identifier.
-          */
-         public:
-
-            enum { Alignment = 4 * 1024 };
-
-            NlcbsmmChunkHeap() :
-               /**
-                *
-                */
-               buffer(NULL),
-               count(0) {}
-
-
-            inline void * malloc (size_t sz) {
-               /**
-                *
-                */
-               void* ptr = NULL;
-
-               switch (heap_identifier) {
-
-               case CLONE_ALLOC_HEAP_START:
-                  fprintf(stderr, "PRIVATE_ALLOC_HEAP_START\n");
-                  break;
-
-               case CLONE_HEAP_START:
-                  fprintf(stderr, "PRIVATE_HEAP_START\n");
-                  break;
-
-               case PAGE_TABLE_ALLOC_HEAP_START:
-                  fprintf(stderr, "PAGE_TABLE_ALLOC_HEAP_START\n");
-                  break;
-
-               case PAGE_TABLE_HEAP_START:
-                  fprintf(stderr, "PAGE_TABLE_HEAP_START\n");
-                  break;
-
-               default:
-                  break;
-               }
-
-               ptr = mmap (NULL, sz, HL_MMAP_PROTECTION_MASK, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-
-               if (ptr == MAP_FAILED) {
-                  ptr = NULL;
-               }
-               return ptr;
-            }
-
-
-            inline void free (void * ptr) {
-               /**
-                *
-                */
-               munmap (reinterpret_cast<char *>(ptr), getSize(ptr));
-            }
-
-
-            inline size_t getSize (void * ptr) {
-               /**
-                *
-                */
-               ptr = ptr;
-               //abort();
-               return Alignment; // Obviously broken. Such is life.
-            }
-
-
-            void free (void * ptr, size_t sz) {
-               /**
-                *
-                */
-               if ((long) sz < 0) {
-                  abort();
-               }
-
-               munmap (reinterpret_cast<char *>(ptr), sz);
-            }
-         private:
-
-            unsigned char* buffer;
-
-            unsigned int count;
-      };
-
-
-   template<int heap_identifier>
-      class NlcbsmmMmapHeap : public NlcbsmmChunkHeap<heap_identifier> {
-         /**
-          * This is the public interface of the NlcbsmmMmapHeap and is thread-safe.
-          */
-
-         private:
-
+    template<int heap_identifier>
+        class NlcbsmmChunkHeap {
             /**
-             * FIX ME: 16 = size of ZoneHeap header.
+             * The NlcbsmmChunkHeap is a chunk heap that grabs memory from a specific
+             * region of memory, depending on the template heap_identifier.
              */
-            class MyHeap :
-               public LockedHeap<SpinLockType, FreelistHeap<ZoneHeap<NlcbsmmChunkHeap<heap_identifier>, 16384 - 16> > >
-         {};
+            public:
 
-            typedef MyHashMap<void *, size_t, MyHeap> mapType;
+                enum { Alignment = 4 * 1024 };
 
-         protected:
-            mapType MyMap;
-
-            SpinLockType MyMapLock;
-
-         public:
-
-            inline void * malloc (size_t sz) {
-               /**
-                *
-                */
-               void * ptr = NlcbsmmChunkHeap<heap_identifier>::malloc (sz);
-               MyMapLock.lock();
-               MyMap.set (ptr, sz);
-               MyMapLock.unlock();
-               assert (reinterpret_cast<size_t>(ptr) % Alignment == 0);
-               return const_cast<void *>(ptr);
-            }
+                NlcbsmmChunkHeap() :
+                    /**
+                     *
+                     */
+                    buffer(NULL),
+                    count(0) {}
 
 
-            inline size_t getSize (void * ptr) {
-               /**
-                *
-                */
-               MyMapLock.lock();
-               size_t sz = MyMap.get (ptr);
-               MyMapLock.unlock();
-               return sz;
-            }
+                inline void * malloc (size_t sz) {
+                    /**
+                     *
+                     */
+                    void* ptr = NULL;
+
+                    //IF the buffer is NULL (First malloc call)
+                    if(buffer == NULL)
+                    {
+                        //SETUP the buffer based on which heap this allocator is for
+                        switch (heap_identifier) {
+
+                        case CLONE_ALLOC_HEAP_START:
+                            fprintf(stderr, "CLONE_ALLOC_HEAP_START\n");
+                            buffer = mmap(CLONE_ALLOC_HEAP_OFFSET, CLONE_ALLOC_HEAP_SZ,
+                                    PROT_EXEC | PROT_READ | PROT_WRITE, MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+                            break;
+
+                        case CLONE_HEAP_START:
+                            fprintf(stderr, "CLONE_HEAP_START\n");
+                            buffer = mmap(CLONE_HEAP_OFFSET, CLONE_HEAP_SZ,
+                                    PROT_EXEC | PROT_READ | PROT_WRITE, MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+                            break;
+
+                        case PAGE_TABLE_ALLOC_HEAP_START:
+                            fprintf(stderr, "PAGE_TABLE_ALLOC_HEAP_START\n");
+                            buffer = mmap(PAGE_TABLE_ALLOC_HEAP_OFFSET, PAGE_TABLE_ALLOC_HEAP_SZ,
+                                    PROT_EXEC | PROT_READ | PROT_WRITE, MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+                            break;
+
+                        case PAGE_TABLE_HEAP_START:
+                            fprintf(stderr, "PAGE_TABLE_HEAP_START\n");
+                            buffer = mmap(PAGE_TABLE_HEAP_OFFSET, PAGE_TABLE_HEAP_SZ,
+                                    PROT_EXEC | PROT_READ | PROT_WRITE, MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+                            break;
+
+                        default:
+                            fprintf(stderr, "UNKOWN heap_indentifier\n");
+                            break;
+                        }
+                        count = 0;
+                    }
+
+                    //IF the mmap failed return null
+                    if (buffer == MAP_FAILED) {
+                        ptr = NULL;
+                    }
+                    //OTHERWISE
+                    else 
+                    {
+                        //RETURN a ptr to a new piece of memory
+                        ptr = buffer;
+                        buffer = reinterpret_cast<void *>(reinterpret_cast<uint32_t>(buffer) + sz);
+                        count++;
+                    }
+                    return ptr;
+                }
 
 
-            void free (void * ptr, size_t sz) {
-               /**
-                * WORKAROUND: apparent gcc bug.
-                */
-               NlcbsmmChunkHeap<heap_identifier>::free (ptr, sz);
-            }
+                inline void free (void * ptr) {
+                    /**
+                     *
+                     */
+                    munmap (reinterpret_cast<char *>(ptr), getSize(ptr));
+                }
 
 
-            inline void free (void * ptr) {
-               /**
-                *
-                */
-               assert (reinterpret_cast<size_t>(ptr) % Alignment == 0);
-               MyMapLock.lock();
-               size_t sz = MyMap.get (ptr);
-               NlcbsmmChunkHeap<heap_identifier>::free (ptr, sz);
-               MyMap.erase (ptr);
-               MyMapLock.unlock();
-            }
-      };
+                inline size_t getSize (void * ptr) {
+                    /**
+                     *
+                     */
+                    ptr = ptr;
+                    //abort();
+                    return Alignment; // Obviously broken. Such is life.
+                }
+
+
+                void free (void * ptr, size_t sz) {
+                    /**
+                     *
+                     */
+                    if ((long) sz < 0) {
+                        abort();
+                    }
+
+                    munmap (reinterpret_cast<char *>(ptr), sz);
+                }
+            private:
+
+                void* buffer;
+
+                unsigned int count;
+        };
+
+
+    template<int heap_identifier>
+        class NlcbsmmMmapHeap : public NlcbsmmChunkHeap<heap_identifier> {
+            /**
+             * This is the public interface of the NlcbsmmMmapHeap and is thread-safe.
+             */
+
+            private:
+
+                /**
+                 * FIX ME: 16 = size of ZoneHeap header.
+                 */
+                class MyHeap :
+                    public LockedHeap<SpinLockType, FreelistHeap<ZoneHeap<NlcbsmmChunkHeap<heap_identifier>, 16384 - 16> > >
+            {};
+
+                typedef MyHashMap<void *, size_t, MyHeap> mapType;
+
+            protected:
+                mapType MyMap;
+
+                SpinLockType MyMapLock;
+
+            public:
+
+                inline void * malloc (size_t sz) {
+                    /**
+                     *
+                     */
+                    void * ptr = NlcbsmmChunkHeap<heap_identifier>::malloc (sz);
+                    MyMapLock.lock();
+                    MyMap.set (ptr, sz);
+                    MyMapLock.unlock();
+                    assert (reinterpret_cast<size_t>(ptr) % Alignment == 0);
+                    return const_cast<void *>(ptr);
+                }
+
+
+                inline size_t getSize (void * ptr) {
+                    /**
+                     *
+                     */
+                    MyMapLock.lock();
+                    size_t sz = MyMap.get (ptr);
+                    MyMapLock.unlock();
+                    return sz;
+                }
+
+
+                void free (void * ptr, size_t sz) {
+                    /**
+                     * WORKAROUND: apparent gcc bug.
+                     */
+                    NlcbsmmChunkHeap<heap_identifier>::free (ptr, sz);
+                }
+
+
+                inline void free (void * ptr) {
+                    /**
+                     *
+                     */
+                    assert (reinterpret_cast<size_t>(ptr) % Alignment == 0);
+                    MyMapLock.lock();
+                    size_t sz = MyMap.get (ptr);
+                    NlcbsmmChunkHeap<heap_identifier>::free (ptr, sz);
+                    MyMap.erase (ptr);
+                    MyMapLock.unlock();
+                }
+        };
 }
 #endif
