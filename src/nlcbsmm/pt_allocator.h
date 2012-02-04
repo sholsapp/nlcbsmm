4132c11bce911845b830804754be37848721e1d6
Stephen Holsapple <sholsapp@gmail.com>
2012-02-04 14:17:47 -0800

    Removed stderr output from custom allocators -- it's obstructive.

--- src/nlcbsmm/pt_allocator.h
+++ src/nlcbsmm/pt_allocator.h
@@ -62,11 +62,11 @@ namespace NLCBSMM {
             /**
              * Allocate but don't initialize num elements of type T.
              */
-            std::cerr << "allocate " << num << " element(s)"
-               << " of size " << sizeof(T) << std::endl;
+            //std::cerr << "allocate " << num << " element(s)"
+            //   << " of size " << sizeof(T) << std::endl;
             // The heap is coupled global variabled -- must be defined!
             pointer ret = (pointer)(pt_heap.malloc(num*sizeof(T)));
-            std::cerr << " allocated at: " << (void*)ret << std::endl;
+            //std::cerr << " allocated at: " << (void*)ret << std::endl;
             return ret;
          }
 
@@ -91,7 +91,7 @@ namespace NLCBSMM {
             /**
              * Deallocate storage p of deleted elements.
              */
-            std::cerr << "deallocate " << num << " element(s)"
+            //std::cerr << "deallocate " << num << " element(s)"
                << " of size " << sizeof(T)
                << " at: " << (void*)p << std::endl;
             // The heap is coupled global variabled -- must be defined!
