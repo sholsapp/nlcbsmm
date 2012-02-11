54c8b75f3967dabe65960c86a682a41051578d96
Stephen Holsapple <sholsapp@gmail.com>
2012-02-11 11:48:01 -0800

    Oops - stupid.

--- src/nlcbsmm/firstfitheap.h
+++ src/nlcbsmm/firstfitheap.h
@@ -68,7 +68,7 @@ class FirstFitHeap : public Super {
       inline void free (void * ptr) {
          fprintf(stderr, ">> free(%d) = %p\n", Super::getSize((void*) ptr), ptr);
          for (int i = 0; i < Super::getSize((void*) ptr); i++) {
-            ((uint32_t*) ptr)[i] = 0xAAAABBBB;
+            ((uint8_t*) ptr)[i] = 0xAB;
          }
 
          // Add this object to the free list.
