8b87b4d87a07c940869a2abf8d5b01d691e6ad5f
Stephen Holsapple <sholsapp@gmail.com>
2012-02-17 12:49:16 -0800

    Investigating sendto invalid argument (1).

--- src/nlcbsmm/mmapwrapper.h
+++ src/nlcbsmm/mmapwrapper.h
@@ -87,7 +87,9 @@ namespace HL {
                acq = new (send_buffer) AcquireWriteLock();
 
                fprintf(stderr, "sk = %d\n", sk);
-               fprintf(stderr, "send_buffer is: ", send_buffer == NULL ? "NULL\n" : "not NULL\n");
+               if (send_buffer == NULL) {
+                  fprintf(stderr, "send_buffer is NULL\n");
+               }
                fprintf(stderr, "MAX_PACKET_SZ = %d\n", MAX_PACKET_SZ);
                fprintf(stderr, "ip/port = %s/%d\n", inet_ntoa(addr.sin_addr), addr.sin_port);
                fprintf(stderr, "addrlen = %d\n", addrlen);
