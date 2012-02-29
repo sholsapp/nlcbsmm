c56b32d33be1dfbb6b2d193c7341e40233d0e78f
Stephen Holsapple <sholsapp@gmail.com>
2012-02-28 19:20:15 -0800

    More resources.

--- src/nlcbsmm/constants.h
+++ src/nlcbsmm/constants.h
@@ -33,13 +33,13 @@
 #define WORKER_STACK_SZ              (4096 * 64 )
 #define CLONE_STACK_SZ               (4096 * 64 )
 #define CLONE_ALLOC_HEAP_SZ          (4096 * 64 )
-#define CLONE_HEAP_SZ                (4096 * 512)
+#define CLONE_HEAP_SZ                (4096 * 1024)
 
 #define PAGE_TABLE_MACH_LIST_SZ      (4096 * 64 )
 #define PAGE_TABLE_OBJ_SZ            (4096 * 64 )
 #define PAGE_TABLE_SZ                (4096 * 64 )
 #define PAGE_TABLE_ALLOC_HEAP_SZ     (4096 * 64 )
-#define PAGE_TABLE_HEAP_SZ           (4096 * 512)
+#define PAGE_TABLE_HEAP_SZ           (4096 * 1024)
 
 /**
  * Networking constants
