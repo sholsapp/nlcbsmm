7a6407bbb807f39ecda9e9e37b2cc36a1d482f36
Stephen Holsapple <sholsapp@gmail.com>
2012-02-29 19:11:33 -0800

    That was bad

--- src/nlcbsmm/mutex.cpp
+++ src/nlcbsmm/mutex.cpp
@@ -1,7 +1,5 @@
 #include "mutex.h"
 
-#include <cstdio>
-
 struct timespec TIMESPEC = { INT_MAX, 0 };
 
 extern "C" {
@@ -34,8 +32,6 @@ extern "C" {
       int i;
       int c;
 
-      fprintf(stderr, "> lock\n");
-
       // Spin and try to take lock
       for (i = 0; i < 100; i++) {
          c = cmpxchg(m, 0, 1);
@@ -58,8 +54,6 @@ extern "C" {
 
    int mutex_unlock(mutex *m) {
       int i;
-
-      fprintf(stderr, "> unlock\n");
 
       // Unlock, and if not contended then exit
       if (*m == 2) {
