3abb2d563ff65d712cd588f815c578f16345d6a3
Stephen Holsapple <sholsapp@gmail.com>
2012-02-29 22:45:11 -0800

    Changing magic value.

--- src/nlcbsmm/firstfitheap.h
+++ src/nlcbsmm/firstfitheap.h
@@ -69,7 +69,7 @@ class FirstFitHeap : public Super {
 
          //fprintf(stderr, ">> free(%d) = %p\n", Super::getSize((void*) ptr), ptr);
          for (int i = 0; i < Super::getSize((void*) ptr); i++) {
-            ((uint8_t*) ptr)[i] = 0xAB;
+            ((uint8_t*) ptr)[i] = 0xED;
          }
 
          // Add this object to the free list.
