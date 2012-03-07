22f6095bbeaff74755a10984c816df3ccab7a211
Stephen Holsapple <sholsapp@gmail.com>
2012-03-07 15:43:19 -0800

    I committed errors. Fuck da police.

--- src/nlcbsmm/vmmanager.cpp
+++ src/nlcbsmm/vmmanager.cpp
@@ -176,7 +176,7 @@ namespace NLCBSMM {
       fprintf(stderr, "> %p - permission(%x)\n", (void*) page->address, page->protection);
       if (node->ip_address == local_addr.s_addr) {
          fprintf(stderr, "> %p invalidate - granting PROT_WRITE!\n", (void*) page->address);
-         if(mprotect(page->address,
+         if(mprotect((void*) page->address,
                   PAGE_SZ,
                   PROT_READ | PROT_WRITE) < 0) {
             fprintf(stderr, "> Fault: mprotect failed\n");
